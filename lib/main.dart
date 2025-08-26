c46e62d6ea9a982a470bd05c3e4c20b17786471f
MD.Prins Ahad <151132033+ahadprince320@users.noreply.github.com>
2025-08-26 07:03:47 -0700

    Fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_tutorial/provider/count_provider.dart';
+import 'package:provider_tutorial/provider/slider_provider.dart';
 import 'package:provider_tutorial/sreen/home_screen.dart';
 import 'package:provider_tutorial/sreen/home_screen_2.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  
   @override
   State<MyApp> createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create:(context) => CountProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CountProvider()),
+        ChangeNotifierProvider(create: (context) => sliderProvider()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: homescreen_two(),
       ),
-
     );
-
   }
 }
--- lib/sreen/home_screen_2.dart
+++ lib/sreen/home_screen_2.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_tutorial/provider/slider_provider.dart';
+
+///it's use of setstate satemanagement
+
+/*import 'package:flutter/material.dart';
 
 class homescreen_two extends StatefulWidget {
   const homescreen_two({super.key});
@@ -38,6 +44,59 @@ class _homescreen_twoState extends State<homescreen_two> {
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+}
+*/
+
+class homescreen_two extends StatefulWidget {
+  const homescreen_two({super.key});
+
+  @override
+  State<homescreen_two> createState() => _homescreen_twoState();
+}
+
+class _homescreen_twoState extends State<homescreen_two> {
+  @override
+  Widget build(BuildContext context) {
+    final sliderprovider = Provider.of<sliderProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(title: Text('Provider tuorial')),
+      body: Column(
+        children: [
+          Consumer<sliderProvider>(
+            builder: (BuildContext context, value, Widget? child) {
+              return Slider(
+                value: sliderprovider.value,
+                onChanged: (val) {
+                  sliderprovider.setValue(val);
+                },
+              );
+            },
+          ),
+          Consumer<sliderProvider>(builder: (context, value, child) {
+           return Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(color: Colors.green.withOpacity(value.value)),
+                    child: Text('Container 1'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(color: Colors.red.withOpacity(value.value)),
+                    child: Text('Container 1'),
+                  ),
+                ),
+              ],
+            );
+            
+          },)
         ],
       ),
     );
